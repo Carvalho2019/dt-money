ebd79ff3b91f7911caa40a1cfbe046e9109affdf
Carvalho2019 <taisomcarvalho15@gmail.com>
2022-05-12 09:18:30 +0100

    create design of TransitionTable  components

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="title">Desenvolvimento de web site</td>
+            <td className="deposit">1200,00 kzs</td>
+            <td>Desenvolvimento</td>
+            <td>20/2/2021</td>
+          </tr>
+          <tr>
+            <td>Alguel de web site</td>
+            <td className="withdraw">100,00 kzs</td>
+            <td>Casa</td>
+            <td>10/2/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
